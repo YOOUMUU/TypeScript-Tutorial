50f424abf9f0847c3f7dc4812c6a1493eae77dcf
YOOUMUU <yooumuu@gmail.com>
2023-04-19 02:22:14 +0800

    Generics

--- public/app.js
+++ public/app.js
@@ -36,3 +36,48 @@ form.addEventListener("submit", (e) => {
     }
     list.render(doc, type.value, "end");
 });
+// Generics
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let doc1 = addUID({ name: "yoshi", age: 40 });
+console.log(doc1);
+// console.log(docOne.name); // error，因为上面没有声明变量会有什么属性，所以ts不知道是否会存在name的属性
+// 加入泛型，ts会捕获对象有什么属性
+const addUID2 = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let doc2 = addUID2({ name: "yoshi", age: 40 });
+let doc3 = addUID2("hello"); // 但是没有阻止传入不想要的类型
+console.log(doc2.name);
+console.log(doc3);
+// 加入extends关键词来规定传入的类型
+const addUID3 = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let doc4 = addUID3({ name: "yoshi", age: 40 });
+// let doc5 = addUID3("hello"); // error，阻止了传入不想要的类型
+// 指定传入的对象必须要有name的属性，而且必须是string
+const addUID4 = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let doc6 = addUID4({ name: "yoshi", age: 40 });
+const doc7 = {
+    uid: 1,
+    resourceName: "person",
+    data: { name: "shaun" }, // 一定得是对象
+};
+const doc8 = {
+    uid: 2,
+    resourceName: "person",
+    data: ["haha", "wuwu"],
+};
+const doc9 = {
+    uid: 2,
+    resourceName: "person",
+    data: { name: "shaun" },
+};
--- src/app.ts
+++ src/app.ts
@@ -49,3 +49,77 @@ form.addEventListener("submit", (e: Event) => {
 
   list.render(doc, type.value, "end");
 });
+
+// Generics
+
+const addUID = (obj: object) => {
+  let uid = Math.floor(Math.random() * 100);
+  return { ...obj, uid };
+};
+
+let doc1 = addUID({ name: "yoshi", age: 40 });
+
+console.log(doc1);
+// console.log(docOne.name); // error，因为上面没有声明变量会有什么属性，所以ts不知道是否会存在name的属性
+
+// 加入泛型，ts会捕获对象有什么属性
+const addUID2 = <T>(obj: T) => {
+  let uid = Math.floor(Math.random() * 100);
+  return { ...obj, uid };
+};
+
+let doc2 = addUID2({ name: "yoshi", age: 40 });
+let doc3 = addUID2("hello"); // 但是没有阻止传入不想要的类型
+
+console.log(doc2.name);
+console.log(doc3);
+
+// 加入extends关键词来规定传入的类型
+const addUID3 = <T extends object>(obj: T) => {
+  let uid = Math.floor(Math.random() * 100);
+  return { ...obj, uid };
+};
+
+let doc4 = addUID3({ name: "yoshi", age: 40 });
+// let doc5 = addUID3("hello"); // error，阻止了传入不想要的类型
+
+// 指定传入的对象必须要有name的属性，而且必须是string
+const addUID4 = <T extends { name: string }>(obj: T) => {
+  let uid = Math.floor(Math.random() * 100);
+  return { ...obj, uid };
+};
+
+let doc6 = addUID4({ name: "yoshi", age: 40 });
+// let doc7 = addUID4({ age: 40 }); //errror
+
+// generics with interfaces
+interface Resource {
+  uid: number;
+  resourceName: string;
+  data: object; // 想让这个灵活变动，通用
+}
+
+const doc7: Resource = {
+  uid: 1,
+  resourceName: "person",
+  data: { name: "shaun" }, // 一定得是对象
+};
+
+// data的type会是我们创建这个对象时指定的任何类型
+interface Resource2<T> {
+  uid: number;
+  resourceName: string;
+  data: T;
+}
+
+const doc8: Resource2<string[]> = {
+  uid: 2,
+  resourceName: "person",
+  data: ["haha", "wuwu"],
+};
+
+const doc9: Resource2<object> = {
+  uid: 2,
+  resourceName: "person",
+  data: { name: "shaun" },
+};
