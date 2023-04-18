222bdf211f059b61496e0e9d4929c1c157d5cc15
YOOUMUU <yooumuu@gmail.com>
2023-04-19 04:18:49 +0800

    final

--- src/app.ts
+++ src/app.ts
@@ -3,27 +3,6 @@ import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payment.js";
 import { HasFormatter } from "./interfaces/HasFormatter.js";
 
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-
-// docOne = new Invoice("yoshi", "web work", 250);
-// docTwo = new Payment("mario", "plumbing worrk", 200);
-
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-
-// const invOne = new Invoice("mario", "work on the mario website", 250);
-// const invTwo = new Invoice("luigi", "work on the luigi website", 300);
-
-// let invoices: Invoice[] = [];
-// invoices.push(invOne);
-// invoices.push(invTwo);
-
-// invoices.forEach((inv) => {
-//   console.log(inv.client, inv.amount, inv.format());
-// });
-
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
 // inputs
@@ -52,16 +31,3 @@ form.addEventListener("submit", (e: Event) => {
 
   list.render(doc, type.value, "end");
 });
-
-// Tuples
-// array是不用管类型的顺序的
-let arr = ["ryu", 25, true];
-arr[0] = false;
-arr = [30, false, "yoshi"];
-
-let tup: [string, number, boolean] = ["ryu", 35, true];
-tup[0] = "ken";
-// tup[0] = 30; // error
-
-let student: [string, number];
-student = ["shun-li", 234];
